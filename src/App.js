d0328f81a9e79ecb4f5809a3db7fdd395a11ae0c
htduyen <thanhduyeny1@gmail.com>
2022-07-09 21:21:15 +0700

    One-way binding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const gifts = [
+    "Iphone 13",
+    "Samsung S21",
+    "Oppo reno 6"
+  ]
+
+  const [ward, setWard] = useState()
+
+  const onChangeHandler = () => {
+    setWard(gifts[Math.floor(Math.random() * gifts.length)])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>{ward || "Chưa có phần thưởng"}</h1>
+        <button onClick={onChangeHandler}>Cập nhật</button>
+      </div>
     </div>
   );
 }
