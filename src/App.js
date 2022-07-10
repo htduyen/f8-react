8ea684964da47f19d4b9084fc2034cbab61b3d2c
htduyen <thanhduyeny1@gmail.com>
2022-07-10 17:44:47 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
 import { useState } from "react";
 
 function App() {
-  const courses = [
-    { id: 1, name: "ReactJS" },
-    { id: 2, name: "Angular" },
-    { id: 3, name: "VueJs" },
-  ];
+  const storageJobs = localStorage.getItem("jobs");
+  const [job, setJob] = useState("");
+  const [jobs, setJobs] = useState(() => {
+    return JSON.parse(storageJobs) ?? [];
+  });
 
-  const [checked, setChecked] = useState([]);
-
-  const onChangeHandler = (id) => {
-    setChecked(prev =>{
-      const isChecked = checked.includes(id)
-
-      if (isChecked){
-          return [...prev].filter(item => item !== id)
-      }
-      else{
-          return [...prev, id]
-      }
-    }
-      
-    )
-    console.log(checked)
+  const onChangeHandler = () => {
+    setJobs((prev) => {
+     
+    const newJobs = [...prev, job];
+    localStorage.setItem("jobs", JSON.stringify(newJobs));
+    return newJobs;
+    });
+    setJob("");
   };
 
-  // const updateHandler = () => {
-
-  // }
   return (
     <div className="App">
       <div>
-        {courses.map((course) => (
-          <div key={course.id}>
-            <input
-              type="checkbox"
-              checked={checked.includes(course.id)} 
-              onChange={() => onChangeHandler(course.id)}
-            />{course.name}
-          </div>
-        ))}
-        <button >Cập nhật</button>
+        <input value={job} onChange={(e) => setJob(e.target.value)} />
+        <button onClick={onChangeHandler}>Cập nhật</button>
       </div>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
     </div>
   );
 }
