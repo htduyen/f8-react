859da56b19e6afe9c68faf59816026867cec0cd7
htduyen <thanhduyeny1@gmail.com>
2022-07-11 00:08:45 +0700

    Using useEffect + event listener + clean up in useEffect

--- src/Content.js
+++ src/Content.js
@@ -7,6 +7,7 @@ const Content = () => {
 	const [title, setTitle] = useState("");
 
 	const [posts, setPosts] = useState([]);
+	const [goToTop, setGoToTop] = useState(false);
 
 	useEffect(() => {
 		fetch(`https://jsonplaceholder.typicode.com/${type}`)
@@ -16,10 +17,41 @@ const Content = () => {
 			});
 	}, [type]);
 
+	useEffect(() => {
+		const scrollHandler = () => {
+			if (window.scrollY > 200) {
+				setGoToTop(true);
+				// console.log("value");
+			} else {
+				setGoToTop(false);
+			}
+
+			//setGoToTop(window.scrollY > 200)
+		};
+		window.addEventListener("scroll", scrollHandler);
+		console.log('Add listener!!!!!!!!!!!!')
+
+		///Clean up
+		return () => {
+			window.removeEventListener('scroll', scrollHandler)
+			console.log('Clean up!!!!!!!!!!!!!!11')
+		}
+	}, []);
+
+	// console.log('re-render')
+
 	return (
 		<div>
 			{tabs.map((tab) => (
-				<button key={tab} onClick={() => setType(tab)} style={type === tab ? {color: "white", backgroundColor: "black"} : {}}>
+				<button
+					key={tab}
+					onClick={() => setType(tab)}
+					style={
+						type === tab
+							? { color: "white", backgroundColor: "black" }
+							: {}
+					}
+				>
 					{tab}
 				</button>
 			))}
@@ -30,6 +62,7 @@ const Content = () => {
 					<li key={post.id}>{post.title || post.name}</li>
 				))}
 			</ul>
+			{goToTop && <button style={{position: 'fixed', right: 10, bottom: 20}}>Go to top</button>}
 		</div>
 	);
 };
