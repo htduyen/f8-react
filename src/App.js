cf9d74e90a2131f3fd90102071ede976fcd98e05
htduyen <thanhduyeny1@gmail.com>
2022-07-11 23:14:08 +0700

    Using useEffect load img + useEffect with only return to clean up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { useState } from "react";
-import Content from "./Content";
-import Interval from "./Interval";
+import { useEffect, useState } from "react";
 
 function App() {
-  
-  const [show, setShow] = useState(false)
+	const [avatar, setAvatar] = useState();
 
-  const showHandler = () => {
-    setShow(!show)
-  }
+	useEffect(() => {
+		return () => {
+			avatar && URL.revokeObjectURL(avatar.url);
+		};
+	}, [avatar]);
 
-  return (
-    <div className="App">
-        <button onClick={showHandler}>Show</button>
-        {/* { show && <Content/>} */}
-        { show && <Interval/>}
-    </div>
-  );
+	const choseImgHandler = (e) => {
+		const file = e.target.files[0];
+		file.url = URL.createObjectURL(file);
+		setAvatar(file);
+	};
+
+	return (
+		<div className="App">
+			<input type="file" onChange={choseImgHandler} />
+
+			{avatar && <img src={avatar.url} alt="" width="50%" />}
+		</div>
+	);
 }
 
 export default App;
